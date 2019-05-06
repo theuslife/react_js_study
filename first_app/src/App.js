0089f7da25f65f4d4a005ed2c7913c4990db8b57
Matheus Rodrigues <matheusrodrigues@MacBook-Pro-de-Fernando-3.local>
2019-05-06 20:03:16 -0300

    matheus

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -1,31 +1,81 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-const app = props => {
-  const [statePersons, setPersonsState] = useState([
-    { name: "Matheus", age: "20" },
-    { name: "Kleyton", age: "20" },
-    { name: "Vitoria", age: "20" }
-  ]);
+class app extends Component {
 
-  const switchNameHandler = () =>
-    setPersonsState([
-      { name: "Matheus", age: "23" },
-      { name: "Kleyton", age: "30" },
-      { name: "Vitoria", age: "30" }
-    ]);
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [
+        { id: 1, name: "Matheus", age: "20" },
+        { id: 2, name: "Kleyton", age: "20" },
+        { id: 3, name: "Vitoria", age: "20" }
+      ]
+    }
+  }
 
-  const persons = statePersons.map(item => (
-    <Person name={item.name} age={item.age} />
-  ));
+  switchNameHandler = () => {
+    this.setState({
+      data: [
+        { id: 1, name: "Matheus", age: "23" },
+        { id: 2, name: "Kleyton", age: "30" },
+        { id: 3, name: "Vitoria", age: "30" }
+      ]
+    })
+  }
 
-  return (
-    <div className="App">
-      {persons}
-      <button onClick={switchNameHandler}>New person</button>
-    </div>
-  );
-};
+  onChange = (e, id) => {
+
+    const { data } = this.state;
+
+    this.setState({
+      data: data.map(item => {
+        if (item.id === id) {
+          item.name = e.target.value
+          return item
+        } else {
+          return item
+        }
+      })
+    })
+
+    // let newData = data.find(item => (item.id === id) ? item : null)
+
+    // let newData = data;
+
+    // newData.splice(index, 1)
+
+    // console.log(newData)
+
+    // this.setState({
+    //   data: [
+
+    //   ]
+    // })
+    // console.log(e.target.value)
+  }
+
+  render() {
+
+    const { data } = this.state;
+
+
+    const persons = data.map((item, index) => (
+      <Person name={item.name} age={item.age} onChange={(e) => this.onChange(e, item.id)} key={index} value={item.name} />
+    ));
+
+    return (
+      <div className="App">
+        {persons}
+        <button onClick={this.switchNameHandler}>New person</button>
+      </div>
+    )
+
+  }
+
+}
+
+
 
 export default app;
--- first_app/src/Person/Person.js
+++ first_app/src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from "react";
+import './Person.css'
 
 export const person = props => (
-  <div>
+  <div className='Person'>
     <p>
       Nome: {props.name}, idade: {props.age}
     </p>
     <p>{props.children}</p>
+    <input type='text' onChange={(e) => props.onChange(e)} value={props.value} ></input>
   </div>
 );
 
